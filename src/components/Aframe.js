3f08bcdb2f71eff3d00067a836c566153204697c
daisuke-awaji <daisuke.awaji@i.softbank.jp>
2019-01-24 21:20:41 +0900

    aframe

--- src/components/Aframe.js
+++ src/components/Aframe.js
@@ -2,61 +2,52 @@ import React, { Component } from 'react'
 import 'aframe'
 import { Animation, Entity, Scene, } from 'aframe-react'
 
+import 'aframe-animation-component';
+import 'aframe-particle-system-component';
+import 'babel-polyfill';
+
 class Aframe extends Component {
     constructor(props) {
-        super(props)
-        this.state = {
-            degree: 90,
-            position: "0 10 30",
-            rotation: "0 90 0",
-            rotationY: 90,
-        }
-
-        this.rotate = this.rotate.bind(this)
+        super(props);
+        this.state = { color: 'red' };
     }
 
-    componentDidMount() {
-        this.rotate()
-    }
-
-    componentWillUnmount() {
-        if (this.frameHandle) {
-            cancelAnimationFrame(this.frameHandle)
-            this.frameHandle = null
-        }
-    }
-
-    rotate() {
-        let newDegree = this.state.degree + 0.1
-        if (newDegree >= 360) {
-            newDegree -= 360
-        }
-
-        const afterX = 50 * Math.cos(newDegree * (Math.PI / 180));
-        const afterZ = 50 * Math.sin(newDegree * (Math.PI / 180));
-
-        let newRotationY = this.state.rotationY - 0.1
-        if (newRotationY <= -360) {
-            newRotationY += 360
-        }
-
+    changeColor() {
+        const colors = ['red', 'orange', 'yellow', 'green', 'blue'];
         this.setState({
-            degree: newDegree,
-            position: `${afterX} 0 ${afterZ}`,
-            rotation: `${afterZ} ${newRotationY} ${afterX}`,
-            rotationY: newRotationY,
+            color: colors[Math.floor(Math.random() * colors.length)]
         });
-        this.frameHandle = requestAnimationFrame(this.rotate)
     }
 
     render() {
         return (
             <Scene>
-                <Entity primitive='a-sky' src='[360度画像]' />
-                <Entity primitive='a-sphere' color="green" position="-2 10 -3" />
-                <Entity geometry={{ primitive: 'box', width: 5 }} position="0 0 -5" />
-                <Entity rotation={this.state.rotation}>
-                    <Entity primitive='a-camera' position={this.state.position} />
+                <a-assets>
+                    <img id="groundTexture" src="https://cdn.aframe.io/a-painter/images/floor.jpg" />
+                    <img id="skyTexture" src="https://cdn.aframe.io/a-painter/images/sky.jpg" />
+                </a-assets>
+
+                <Entity primitive="a-plane" src="#groundTexture" rotation="-90 0 0" height="100" width="100" />
+                <Entity primitive="a-light" type="ambient" color="#445451" />
+                <Entity primitive="a-light" type="point" intensity="2" position="2 4 4" />
+                <Entity primitive="a-sky" height="2048" radius="30" src="#skyTexture" theta-length="90" width="2048" />
+                <Entity particle-system={{ preset: 'snow', particleCount: 2000 }} />
+                <Entity text={{ value: 'Experience of Chaos Engineering!', align: 'center' }} position={{ x: 0, y: 2, z: -1 }} />
+
+                <Entity id="box"
+                    geometry={{ primitive: 'box' }}
+                    material={{ color: this.state.color, opacity: 0.6 }}
+                    animation__rotate={{ property: 'rotation', dur: 2000, loop: true, to: '360 360 360' }}
+                    animation__scale={{ property: 'scale', dir: 'alternate', dur: 100, loop: true, to: '1.1 1.1 1.1' }}
+                    position={{ x: 0, y: 1, z: -3 }}
+                    events={{ click: this.changeColor.bind(this) }}>
+                    <Entity animation__scale={{ property: 'scale', dir: 'alternate', dur: 100, loop: true, to: '2 2 2' }}
+                        geometry={{ primitive: 'box', depth: 0.2, height: 0.2, width: 0.2 }}
+                        material={{ color: '#24CAFF' }} />
+                </Entity>
+
+                <Entity primitive="a-camera">
+                    <Entity primitive="a-cursor" animation__click={{ property: 'scale', startEvents: 'click', from: '0.1 0.1 0.1', to: '1 1 1', dur: 150 }} />
                 </Entity>
             </Scene>
         )
